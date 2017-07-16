93e2ac3f4cb8291eb5a11af2c5a7df305a13fe31
hzwangying1 <hzwangying1@corp.netease.com>
2017-07-16 18:14:23 +0800

    change:修改selecter名字

---
+++ docs/components/check/check.component.ts
@@ -0,0 +1,8 @@
+import {
+    Component,
+} from '@angular/core';
+
+@Component({
+    templateUrl: './check.component.html',
+})
+export class CssCheckComponent {};
--- docs/components/selecter/selecter.component.ts
+++
@@ -1,8 +0,0 @@
-import {
-    Component,
-} from '@angular/core';
-
-@Component({
-    templateUrl: './selecter.component.html',
-})
-export class CssSelecterComponent {};
