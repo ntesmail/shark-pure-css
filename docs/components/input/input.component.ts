f7bb3fec8b5c4354a0735180e3e91d85ce31b9fa
whw <m15990176834@163.com>
2017-07-16 14:21:35 +0800

    change: v0.0.2

--- docs/components/input/input.component.ts
+++ docs/components/input/input.component.ts
@@ -7,7 +7,14 @@ import {
 })
 export class CssInputComponent {
     pager1html:string = '';
+    mdheight = '';
+    smheight = '';
+    mdwidth = '';
+    smwidth = '';
     constructor() {
         this.pager1html = `<input type="text" class="input"/>`;
+        this.smheight = `<input type="text" class="input input-sm"/>`;
+        this.mdwidth = `<input type="text" class="input input-w-md"/>`;
+        this.smwidth = `<input type="text" class="input input-w-sm"/>`;
     }
 };
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var SharkConfig = require('./shark-deploy-conf');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
 var openurl = require('openurl');
 var IP = require('ip');
 
@@ -24,19 +23,6 @@ app.use(webpackHotMiddleware(compiler, {
     reload: true
 }));
 
-//ajax
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-//ajax mock
-app.use(SharkConfig.contextPath + '/xhr', function (req, res) {
-    console.log('request url:' + req.url);
-    var data = path.join(__dirname, 'test/mock/xhr', req.path);
-    if (fs.existsSync(data)) {
-        res.send(fs.readFileSync(data));
-    } else {
-        res.status(404).send('file not exist !');
-    }
-});
 
 //font
 app.use(SharkConfig.contextPath + '/fonts', express.static(path.join(__dirname, 'docs/asset/fonts')));
